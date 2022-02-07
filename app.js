260138c3b7fe0a5a753b07226db027208b26da55
Adolfo Lopez <alopezcia@gmail.com>
2022-02-07 14:07:30 +0100

    main

--- app.js
+++ app.js
@@ -3,25 +3,40 @@ const app = Vue.createApp({
         return {
             dia: new Date(),
             accion: 'Inicio',
-            principio: undefined,
+            principio:  undefined,
             final: undefined,
-            diferencia: 0,
-            suma: 0,
-            acumulado: localStorage.getItem("Acumulado") ? parseInt(localStorage.getItem("Acumulado")) : 0,
-            entradas: localStorage.getItem("Entradas") ? JSON.parse(localStorage.getItem("Entradas")) : [],
+            diferencia:  0,
+            acumulado:  0,
+            entradas: [],
         }
+    },
+    mounted(){
+        this.accion =  localStorage.getItem("Accion") ? localStorage.getItem("Accion") : 'Inicio'
+        this.principio =  localStorage.getItem("Principio") ? new Date(localStorage.getItem("Principio")) : undefined
+        this.final = localStorage.getItem("Final") ? new Date(localStorage.getItem("Final")) : undefined
+        this.diferencia = localStorage.getItem("Diferencia") ? parseInt(localStorage.getItem("Diferencia")) : 0
+        this.acumulado = localStorage.getItem("Acumulado") ? parseInt(localStorage.getItem("Acumulado")) : 0
+        this.entradas = localStorage.getItem("Entradas") ? JSON.parse(localStorage.getItem("Entradas")) : []
     },
     methods: {
         accionClicked(){
             if( this.accion === 'Inicio' ){
                 this.principio = new Date()
+                localStorage.setItem("Principio", this.principio);
                 this.final = undefined
+                localStorage.removeItem("Final")
                 this.accion = 'Final'
+                localStorage.setItem("Accion", this.accion);
+
             } else {
                 this.final = new Date()
+                localStorage.setItem("Final", this.Final);
                 this.diferencia = Math.round(Math.abs(this.final - this.principio)/60000)
+                localStorage.setItem("Diferencia", this.diferencia);
                 this.acumulado += this.diferencia
+                localStorage.setItem("Acumulado", this.acumulado)
                 this.accion = 'Inicio'
+                localStorage.setItem("Accion", this.accion);
             }
         },
         acumularClicked(){
@@ -35,11 +50,18 @@ const app = Vue.createApp({
                 }
                 this.entradas.push(e)
                 localStorage.setItem("Entradas", JSON.stringify(this.entradas))
-                localStorage.setItem("Acumulado", this.acumulado)
                 this.diferencia=0
+                localStorage.setItem("Diferencia", this.diferencia);
                 this.principio=undefined
+                localStorage.setItem("Principio", this.principio);
                 this.final=undefined
+                localStorage.setItem("Final", this.Final);
             }
+        },
+        resetearClicked(){
+            this.entradas = []
+            localStorage.removeItem("Entradas")
+
         }
     }
 })
